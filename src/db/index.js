4c482dafcdf4365050c1cefdc3ea165eafcfde9a
hasibmuhammad <hasibmu99@gmail.com>
2023-12-20 19:08:21 +0600

    Database connection - Done

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MongoDB connection error", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
